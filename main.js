65050172f8acd2755fc7a37ca10bf4af0a46976a
ryoshi6 <149881985+ryoshi6@users.noreply.github.com>
2023-11-04 21:44:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+
+const audioPlayer = document.getElementById('audioPlayer');
+
+window.onload = function() {
+audioPlayer.play();
+};
+
